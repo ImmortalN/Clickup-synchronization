44b1413c30c16966baa4aeb87b6507403a80211e
ImmortalN <126565015+ImmortalN@users.noreply.github.com>
2026-01-23 11:29:53 +0200

    Change pagination limit from 100 to 25

--- synch.py
+++ synch.py
@@ -35,7 +35,7 @@ def rate_limit_sleep(resp):
 
 def test_pagination():
     base_url = f"{INTERCOM_BASE}/internal_articles"
-    params = {"per_page": 100}
+    params = {"per_page": 25}
     page_num = 1
     total_articles = 0
 
